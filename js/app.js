415439f20b7f40ebb67e2504d39619174f8c91f2
Nairul <awhitehurst4@gmail.com>
2017-03-10 15:30:01 -0500

    adam initial commit

---
+++ js/app.js
@@ -0,0 +1,92 @@
+"use strict";
+
+angular
+  .module("gaBook", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .factory("UserFactory", [
+  	"$resource",
+  	UserFactoryFunction
+  ])
+  .controller("UserIndexController", [
+  	"UserFactory",
+  	UserIndexControllerFunction
+  ])
+  .controller("UserShowController", [
+  	"UserFactory",
+  	"$stateParams",
+  	UserShowControllerFunction
+  ])
+  .controller("UserNewController", [
+  	"UserFactory",
+  	UserNewControllerFunction
+  ])
+  .controller("UserEditController", [
+  	"UserFactory",
+  	"$stateParams",
+  	UserEditControllerFunction
+  ])
+
+  function RouterFunction($stateProvider){
+  $stateProvider
+  .state("userIndex", {
+    url: "/users",
+    templateUrl: "js/ng-views/users/index.html",
+    controller: "UserIndexController",
+    controllerAs: "vm"
+  })
+  $stateProvider
+  .state("userNew", {
+    url: "/users/new",
+    templateUrl: "js/ng-views/users/new.html",
+    controller: "UserNewController",
+    controllerAs: "vm"
+  })
+  $stateProvider
+  .state("userShow", {
+    url: "/users/:id",
+    templateUrl: "js/ng-views/users/show.html",
+    controller: "UserShowController",
+    controllerAs: "vm"
+  })
+  $stateProvider
+  .state("userEdit", {
+  	url: "/users/:id/edit",
+  	templateUrl: "js/ng-views/users/edit.html",
+  	controller: "UserEditController",
+  	controllerAs: "vm"
+  })
+}
+
+function UserFactoryFunction( $resource ) {
+  return $resource( "https://gist.githubusercontent.com/dhouston14/648df5bcffca8c195daba3fa03e37354/raw/5329d0d7218016cc07012c74ef26fdf7e00b00a3/data.json" )
+}
+
+function UserIndexControllerFunction( UserFactory ){
+  this.users = UserFactory.query()
+}
+function UserNewControllerFunction( UserFactory ){
+  this.user = new UserFactory()
+  this.create = function(){
+    this.user.$save()
+  }
+}
+
+function UserShowControllerFunction( UserFactory, $stateParams ){
+  this.user = UserFactory.get({id: $stateParams.id})
+}
+
+function UserEditControllerFunction( UserFactory, $stateParams ){
+    this.user = UserFactory.get({id: $stateParams.id});
+    this.update = function(){
+      this.user.$update({id: $stateParams.id})
+    }
+    this.destroy = function(){
+      this.user.$delete({id: $stateParams.id});
+    }
+ }
